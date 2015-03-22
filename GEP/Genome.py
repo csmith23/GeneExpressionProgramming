e334bc43359193bab21eec8f59e4f586bfeaeda0
Coleman Smith <colemansmith21@gmail.com>
2015-03-21 20:39:20 -0400

    fixed genome underscored functions

--- GEP/Genome.py
+++ GEP/Genome.py
@@ -2,32 +2,32 @@ __author__ = 'Coleman'
 
 class Genome():
     def _add(inputs):
-        return sum(inputs)
+        inputs.append(inputs.pop(-2) + inputs.pop(-1))
     def _sub(inputs):
-        return inputs[0] - inputs[1]
+        inputs.append(inputs.pop(-2) - inputs.pop(-1))
     def _mul(inputs):
-        return inputs[0] * inputs[1]
+        inputs.append(inputs.pop(-2) * inputs.pop(-1))
     def _div(inputs):
         if inputs[1] != 0:
-            return inputs[0] / inputs[1]
+            inputs.append(inputs.pop(-2) / inputs.pop(-1))
         else:
-            return "Error"
+            return 1
 
     def _NOT(inputs):
-        return not inputs[0]
+        inputs.append(not inputs.pop(-1))
     def _AND(inputs):
-        return inputs[0] and inputs[1]
+        inputs.append(inputs.pop(-2) and inputs.pop(-1))
     def _OR(inputs):
-        return inputs[0] or inputs[1]
+        inputs.append(inputs.pop(-2) or inputs.pop(-1))
     def _XOR(inputs):
-        return inputs[0] != inputs[1]
+        inputs.append(inputs.pop(-2) is not inputs.pop(-1))
     def _XNOR(inputs):
-        return inputs[0] == inputs[1]
+        inputs.append(inputs.pop(-2) is inputs.pop(-1))
 
     def _exp(inputs):
-        return inputs[0] ** 2
+        inputs.append(inputs.pop(-1) ** 2)
     def _sqrt(inputs):
-        return inputs[0] ** 0.5
+        inputs.append(inputs.pop(-1) ** 0.5)
 
     ARITHMETIC_SET = {"+": (_add, 2), "-": (_sub, 2), "*": (_mul, 2), "/": (_div, 2), "arity": 2}
     BOOLEAN_SET = {"N": (_NOT, 1), "A": (_AND, 2), "O": (_OR, 2), "arity": 2}
