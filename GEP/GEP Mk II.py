a233ccfa1b8e67585da4e0d11f2916ea6260c225
Coleman Smith <colemansmith21@gmail.com>
2015-06-05 16:23:17 -0400

    Eval, append, and inversion
    Chromosome::eval()
    Node::append()
    Chromosome::inversion()
    added homeoticRange back for eval()

--- GEP/GEP Mk II.py
+++ GEP/GEP Mk II.py
@@ -52,16 +52,19 @@ class Chromosome:
         homeoticTails = [genomes[i + len(headLengths)].homeoticTail(homeoticLengths[i], len(headLengths)) for i in range(len(homeoticLengths))]
         self.headRanges = []
         self.tailRanges = []
+        self.homeoticRanges = []
         self.sequence = []
         for i in range(len(heads)):
             self.headRanges.append((len(self.sequence), len(self.sequence) + len(heads[i])))
             self.tailRanges.append((len(self.sequence) + len(heads[i]), len(self.sequence) + len(heads[i]) + len(tails[i])))
+            self.homeoticRanges.append(False)
             self.sequence += heads[i]
             self.sequence += tails[i]
 
         for i in range(len(homeotics)):
             self.headRanges.append((len(self.sequence), len(self.sequence) + len(homeotics[i])))
             self.tailRanges.append((len(self.sequence) + len(homeotics[i]), len(self.sequence) + len(homeotics[i]) + len(homeoticTails[i])))
+            self.homeoticRanges.append(True)
             self.sequence += homeotics[i]
             self.sequence += homeoticTails[i]
 
@@ -70,8 +73,38 @@ class Chromosome:
             for i in range(headRange[0], headRange[1]):
                 self.head[i] = True
 
-    def eval(self):
-        pass
+    def eval(self, terminalInputs):
+        genes = []
+        for headRange, tailRange, homeoticRange in zip(self.headRanges, self.tailRanges, self.homeoticRanges):
+            genes.append((self.sequence[headRange[0]:tailRange[1]], homeoticRange))
+
+        inputs = []
+        for index in range(len(genes)):
+            inputs.append(dict(zip(self.symbols[index][1], terminalInputs)))
+
+        trees = []
+        for index in range(len(genes)):
+            gene = genes[index][0]
+            if isinstance(gene[0], int):
+                node = Node(gene[0], 0)
+            else:
+                node = Node(gene[0], self.genomes[index].arities[gene[0]])
+
+            trees.append((node, genes[index][1]))
+            for symbol in gene[1:]:
+                if isinstance(symbol, int):
+                    node.append(symbol, 0)
+                else:
+                    node.append(symbol, self.genomes[index].arities[symbol])
+
+        homeoticInputs = {}
+        for index in range(len(trees)):
+            if trees[index][1]:
+                homeoticInputs[index] = trees[index][0].eval(self.genomes[index], homeoticInputs)
+            else:
+                homeoticInputs[index] = trees[index][0].eval(self.genomes[index], inputs[index])
+
+        return homeoticInputs
 
     def mutation(self, rate):
         indices = []
@@ -80,10 +113,9 @@ class Chromosome:
 
         random.shuffle(indices)
         indices = indices[:int(round(rate * len(indices)))]
-        print(indices)
         for index in indices:
             if self.head[index[0]]:
-                symbols = self.symbols[index[1]][0][:]
+                symbols = self.symbols[index[1]][0] + self.symbols[index[1]][1]
                 symbols.remove(self.sequence[index[0]])
                 if len(symbols) > 0:
                     self.sequence[index[0]] = random.choice(symbols)
@@ -94,7 +126,10 @@ class Chromosome:
                     self.sequence[index[0]] = random.choice(symbols)
 
     def inversion(self):
-        pass
+        index = random.randint(0, len(self.headRanges) - 1)
+        start = random.randint(self.headRanges[index][0], self.headRanges[index][1] - 1)
+        end = random.randint(start + 1, self.headRanges[index][1])
+        self.sequence[start:end] = reversed(self.sequence[start:end])
 
     def ISTransposition(self):
         pass
@@ -129,6 +164,7 @@ class Genome:
 
     def addTerminal(self, symbol):
         self.terminals.append(symbol)
+        self.arities[symbol] = 0
 
     def head(self, length):
         head = [random.choice(list(self.functions.keys()) + self.terminals) for i in range(length)]
@@ -150,9 +186,9 @@ class Genome:
 
     def symbols(self, numGenes, index):
         if index > numGenes - 1:
-            return (list(self.functions.keys()) + list(range(numGenes)), list(range(numGenes)))
+            return (list(self.functions.keys()), list(range(numGenes)))
 
-        return (list(self.functions.keys()) + self.terminals, self.terminals)
+        return (list(self.functions.keys()), self.terminals)
 
 
     def _add(inputs):
@@ -204,35 +240,48 @@ class Genome:
     EXPONENTIAL_SET = ({"E": _exp, "L": _log}, {"E": 1, "L": 1})
 
 class Node:
-    def __init__(self, symbol):
+    def __init__(self, symbol, arity):
         self.children = []
         self.symbol = symbol
+        self.arity = arity
 
-    def eval(self, genome, terminalInputs, homeoticInputs):
+    def append(self, symbol, arity):
+        if len(self.children) < self.arity:
+            self.children.append(Node(symbol, arity))
+            return True
+        else:
+            for child in self.children:
+                if child.append(symbol, arity):
+                    return True
+
+            return False
+
+    def eval(self, genome, terminalInputs):
         inputs = []
         for child in self.children:
-            inputs.append(child.eval(genome, terminalInputs, homeoticInputs))
+            inputs.append(child.eval(genome, terminalInputs))
 
         if self.symbol in list(genome.functions.keys()):
             return genome.functions[self.symbol](inputs)
-        elif isinstance(self.symbol, int):
-            return homeoticInputs[self.symbol]
 
         return terminalInputs[self.symbol]
 
 
 genome = Genome()
-genome.addFunction("A", None, 2)
-genome.addFunction("O", None, 2)
-genome.addFunction("N", None, 1)
+genome.addFunction("A", Genome._AND, 2)
+genome.addFunction("O", Genome._OR, 2)
+genome.addFunction("N", Genome._NOT, 1)
 genome.addTerminal("a")
 genome.addTerminal("b")
-chromosome = Chromosome([2, 2, 2], [2], [genome for i in range(4)])
+chromosome = Chromosome([5, 5, 5], [5], [genome for i in range(4)])
 print(chromosome.headRanges)
 print(chromosome.tailRanges)
 print(chromosome.homeoticRanges)
 print(chromosome.symbols)
 print(chromosome.head)
 print(chromosome.sequence)
-chromosome.mutation(1/4)
+# chromosome.mutation(1/4)
+chromosome.inversion()
 print(chromosome.sequence)
+output = chromosome.eval([True, False])
+print(output)
